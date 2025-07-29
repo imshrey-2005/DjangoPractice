f37e9eadc40de820829a5ac3e45f8500c7ba8321
imshrey-2005 <shreysmehta2005@gmail.com>
2025-07-29 23:28:17 +0530

    add templates

---
+++ forms/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+class Form1(forms.Form):
+    name=forms.CharField(max_length=100,label="Enter your name")
+    email=forms.EmailField(label="Enter your email")
+    message=forms.CharField(widget=forms.Textarea, label="Enter your message")
+
+    def send_email(self):
+        print(f"Sending email to {self.cleaned_data['email']}")
+        print(f"Message: {self.cleaned_data['message']}")   
--- forms/views.py
+++ forms/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from .forms import Form1
 # Create your views here.
+def home_view(request):
+    return render(request,"home.html")
+def contact_view(request):
+    if request.method =='POST':
+        form= Form1(request.POST)
+        if form.is_valid():
+            form.send_email()
+            return render(request,"success.html",{"form":form})
+        else:
+            form=Form1()
+
+    return render(request,"contact.html",{"form":form})
+
+def success_view(request):
+    return render(request,"success.html")
